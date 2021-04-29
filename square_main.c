dfac2853b87a93727043e5c0b6855e1c596c3981
Ryan Miskoviak <52227685+RJM88@users.noreply.github.com>
2021-04-29 10:48:54 -0600

    Square program uploaded
    C program uploaded

---
+++ square_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int square(int number);
+int main()
+{
+    int count, sq;
+    for (count = 1; count < 11; count++)
+    {
+        sq = square(count);
+        printf("The square of %d is %d\n", count, sq);
+    }
+}
